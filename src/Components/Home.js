5a7af8ef9d183c03be2e3a2b0654e8fe6fb0c41d
Cai Jenkins <Caisjenkins@gmail.com>
2020-02-08 13:56:06 +0000

    added a basic template for the app

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div >
+             <h1><b>Compare your Air</b></h1>
+             <h3>Compare air quality between cities in the UK.</h3>
+             <h3>Select cities to compare using the search tool below.</h3>   
+            </div>        
+            );
+    }
+}
+
+export default Home;
